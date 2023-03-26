6ef4e6930814327111b7498a1aeefb0557d9329a
Katie Ferreol <kmf527@nyu.edu>
2023-03-26 22:28:42 +0400

    Update sketch.js

--- Capstone_Form/static/sketch.js
+++ Capstone_Form/static/sketch.js
@@ -1,122 +1,3 @@
-// class Entry {
-//   constructor(inputText) {
-//     this.x = random(width);
-//     this.y = random(height);
-//     this.v = 10;
-//     this.yVal = 20; 
-//     this.m = 50;
-//     this.a = 50*0.0005;
-//     this.inputText = inputText;
-//   }
-
-//   display() {
-//     fill(255);
-//     textSize(50);
-    
-//     this.v += this.a; 
-//     this.yVal += this.v;
-//     text(this.inputText, this.x, this.yVal); 
-    
-//     if (this.yVal > height - this.m/2) {
-//       // A little dampening when hitting the bottom
-//       this.v *= -0.2;
-//       this.yVal = height - this.m/2;
-//     }
-//   }
-// }
-
-// let xspeed;
-// let yspeed;
-// let inputText;
-// let inputWidth;
-// let inputHeight;
-// var fade;
-// var fadeAmount = 1;
-
-// let table;
-// let values;
-// let splitString;
-// let thoughts = [];
-
-// function preload() {
-//   table = loadTable("static/assets/experience.csv", "csv");
-// }
-
-// function setup() {
-//   createCanvas(windowWidth, windowHeight);
-//   xspeed = 1;
-//   yspeed = 1;
-//   thoughts = table.getArray();
-
-//   for (let i = 0; i < thoughts.length; i++) {
-//     thoughts[i] = new Entry(thoughts[i]);
-//   }
-  
-//   x = random(2*width/6,3*width/6);
-//   y = random(height);
- 
-// }
-
-// function draw() {
-//   background(0);
-
-//   // displayText(thoughts[i]);
-
-//   for (let i = 0; i < thoughts.length; i++) {
-//     thoughts[i].display();
-//     console.log(thoughts[i]);
-//   }
-
-//   if (frameCount%60==0) {
-//     table = loadTable("static/assets/experience.csv", "csv");
-//   }
-  
-// }
-
-// function displayText(inputText) {
-  
-//   text(inputText, x, y);
-//   textSize(40);
-//   fill(255);
-  
-//   inputWidth = textWidth(inputText);
-//   inputHeight = textWidth(inputText);
-  
-//   x = x + xspeed;
-//   y = y + yspeed;
-
-//   if (x + inputWidth >= width) {
-//     xspeed = -xspeed;
-//     x = width - inputWidth;
-//   } else if (x <= 0) {
-//     xspeed = -xspeed;
-//     x = 0;
-//   }
-
-//   if (y + inputHeight >= height) {
-//     yspeed = -yspeed;
-//     y = height - inputHeight;
-//   } else if (y <= 0) {
-//     yspeed = -yspeed;
-//     y = 0;
-//   }
-// }
-
-// function displayText(inputText) {
-//   fill(255);
-//   textSize(50);
-  
-//   velocity += accel; 
-//   yVal += velocity;
-// 	text(inputText, x, yVal); 
-  
-//   if (yVal > height - mass/2) {
-//     // A little dampening when hitting the bottom
-//     velocity *= -0.2;
-//     yVal = height - mass/2;
-//   }
-// }
-
 let world;
 let i;
 
@@ -125,33 +6,42 @@ let boundaries = [];
 // A list for all of our rectangles
 let boxes = [];
 let thoughts = [];
+let language = [];
+let font = [];
 
 function preload() {
-  fontFile = loadFont('/static/assets/Hangul.ttf');
-  table = loadTable("static/assets/experience.csv", "csv");
+  laGreCyFont = loadFont('/static/assets/LaGreCy.ttf');
+  arabicFont = loadFont('/static/assets/Arabic.ttf');
+  hangulFont = loadFont('/static/assets/Hangul.otf');
+  hiraKataFont = loadFont('/static/assets/HiraKata.otf');
+  sHanFont = loadFont('/static/assets/sHan.otf');
+  tHanFont = loadFont('/static/assets/tHan.otf');
+  devangariFont = loadFont('/static/assets/Devangari.ttf');
+  bengaliFont = loadFont('/static/assets/Bengali.ttf');
+  table = loadTable("static/assets/experience.csv", "csv", "header");
 }
 
 function setup() {
   i = -1;
   createCanvas(windowWidth, windowHeight);
+  thoughts = table.getColumn('input');
+  language = table.getColumn('language');
 
   // Initialize box2d physics and create the world
   world = createWorld();
 
   // Add a bunch of fixed boundaries
-  boundaries.push(new Boundary(1.6*windowWidth/5, 370, 30, windowHeight));
-  boundaries.push(new Boundary(3.4*windowWidth/5, 370, 30, windowHeight));
+  //boundaries.push(new Boundary(1.6*windowWidth/5, 370, 30, windowHeight));
+  //boundaries.push(new Boundary(3.4*windowWidth/5, 370, 30, windowHeight));
   boundaries.push(new Boundary(700, 2.6*windowHeight/5, windowWidth, 30));
 
   // let b = new Box(width / 2, 30);
   // boxes.push(b);
-  textFont(fontFile);
-  
 }
 
 function draw() {
+
   background(0);
-  thoughts = table.getArray();
 
   // We must always step through time!
   let timeStep = 1.0 / 30;
@@ -159,29 +49,47 @@ function draw() {
   world.Step(timeStep, 10, 10);
 
   if (frameCount%10==0) {
-    table = loadTable("static/assets/experience.csv", "csv");
+    table = loadTable("static/assets/experience.csv", "csv", "header");
   }
 
   while (i < thoughts.length-1) {
     i++;
-    let b = new Box(thoughts[i], random(2*windowWidth/5,3*windowWidth/5), 0, random(100,255),random(100,255),random(100,255));
+    let b = new Box(thoughts[i], random(2*windowWidth/5,3*windowWidth/5), 0, random(80,255),random(80,255),random(80,255),language[i],language[i]);
     boxes.push(b);
   }
 
   //console.log(i);
 
   // Display all the boundaries
-  // for (let i = 0; i < boundaries.length; i++) {
-  //   boundaries[i].display();
-  // }
+  for (let i = 0; i < boundaries.length; i++) {
+    boundaries[i].display();
+  }
 
   // Display all the boxes
   for (let i = 0; i < boxes.length; i++) {
     boxes[i].display();
   }
 
-  for (let i = boxes.length; i > 5; i--) {
+  for (let i = boxes.length; i > 10; i--) {
     boxes[0].killBody();
     boxes.splice(0, 1);
   }
 }
+
+// if(language[i] == "Latin" || language[i] == "Greek" || language[i] == "Cyrillic") {
+//   textFont(laGreCyFont);
+// } if(language[i] == "Arabic") {
+//   textFont(arabicFont);
+// } if(language[i] == "Hangul") {
+//   textFont(hangulFont);
+// } if(language[i] == "Hiragana/Katakana") {
+//   textFont(hiraKataFont);
+// } if(language[i] == "Simplified Han") {
+//   textFont(sHanFont);
+// } if(language[i] == "Traditional Han") {
+//   textFont(tHanFont);
+// } if(language[i] == "Devangari") {
+//   textFont(devangariFont);
+// } if(language[i] == "Bengali") {
+//   textFont(bengaliFont);
+// }
